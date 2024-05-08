2ffcc94fdc0d16cb96ac3bfca738486c5f14d98d
khai <19130094@st.hcmuaf.edu.vn>
2024-05-08 18:59:16 +0700

    header and footer

--- frontend/src/pages/users/theme/footer/footer.js
+++ frontend/src/pages/users/theme/footer/footer.js
@@ -1,7 +1,76 @@
 import {memo} from "react";
+// import React, { useState } from 'react';
+import "./style.scss"
+import { AiFillFacebook } from "react-icons/ai";
+import { AiOutlineInstagram } from "react-icons/ai";
+import { AiFillGooglePlusCircle } from "react-icons/ai";
+import { AiFillTwitterSquare } from "react-icons/ai";
 
 const Footer = () =>{
-    return <h1>Footer</h1>;
+    return(
+        <footer className="footer">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-3 col-md-6 col-sm-6 col-xs-6">
+                        <div className="footer_about">
+                            <h1 className="footer_about_logo">My Shop</h1>
+                            <ul>
+                                <li>Linh Trung, Thủ Đức, TP.HCM</li>
+                                <li>Phone: 1234567890</li>
+                                <li>Shoprauqua@gmail.com</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                        <div className="footer_widget">
+                            <h6>Thông tin</h6>
+                            <ul>
+                                <li>
+                                    <a href="">Giới thiệu</a>
+                                </li>
+                                <li>
+                                    <a href="">Chứng nhận</a>
+                                </li>
+                                <li>
+                                    <a href="">Sản phẩm kinh doanh</a>
+                                </li>
+
+                            </ul>
+                            <ul>
+                                <li>
+                                    <a href="">Chính sách bảo mật</a>
+                                </li>
+                                <li>
+                                    <a href="">Điều khoản sử dụng</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="col-lg-3 col-md-12 col-sm-12 col-xs-12">
+                        <div className="footer_widget">
+                            <h6>Khuyễn mãi & ưu đãi</h6>
+                            <p>Đăng ký nhận thông tin tại đây</p>
+                            <form action="#">
+                                <div className="input-group">
+                                    <input type="text" placeholder="Nhập email"/>
+                                    <button type="submit" className="button-submit">
+                                        Đăng ký
+                                    </button>
+                                </div>
+                                <div className="footer_widget_social">
+                                        <div><AiFillFacebook /></div>
+                                        <div><AiOutlineInstagram /></div>
+                                        <div><AiFillGooglePlusCircle /></div>
+                                        <div><AiFillTwitterSquare /></div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+
 
 };
 export default memo(Footer);
--- frontend/src/pages/users/theme/header/header.js
+++ frontend/src/pages/users/theme/header/header.js
@@ -6,32 +6,123 @@ import { AiOutlineUser } from "react-icons/ai";
 import { AiFillTwitterSquare } from "react-icons/ai";
 import { AiFillGooglePlusCircle } from "react-icons/ai";
 import { AiOutlineMail } from "react-icons/ai";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import {formatter} from "utils/formatter"
+import {ROUTERS} from "utils/router"
+import {Route, Routes} from "react-router-dom";
+import React, { useState } from 'react';
 const Header = () =>{
+
+    const [menus, setMenus] = useState([
+        {
+            name : "Trang chủ",
+            path :  ROUTERS.USER.HOME
+        },
+        {
+            name : "Cửa hàng",
+            path :  ROUTERS.USER.PRODUCT
+        },
+        {
+            name : "Sản phẩm",
+            path :  ROUTERS.USER.HOME,
+            isShowSubmenu: false,
+            child: [
+                {
+                    name : "Rau củ",
+                    path : ""
+                },
+                {
+                    name : "Trái cây",
+                    path : ""
+                }
+            ]
+
+        },
+        {
+            name : "Bài viết",
+            path :  ROUTERS.USER.HOME
+        },
+        {
+            name : "Liên hệ",
+            path :  ROUTERS.USER.HOME
+        }
+    ])
+
     return (
-        <div className="header-top">
-            <div className="container">
-                <div className="row">
-                    <div className="col-6 header-top-left">
-                        <ul>
-                            <li><AiOutlineMail />Shoprauqua@gmail.com</li>
-                            <li>Miễn phí giao hàng tận nơi!</li>
-                        </ul>
-                    </div>
-                    <div className="col-6 header-top-right">
-                        <ul>
-                            <li><a href=""><AiFillFacebook /></a></li>
-                            <li><a href=""><AiOutlineInstagram /></a></li>
-                            <li><a href=""><AiFillGooglePlusCircle /></a></li>
-                            <li><a href=""><AiFillTwitterSquare /></a></li>
-                            <li>
-                                <a href=""><AiOutlineUser /></a>
-                               <span> Đăng nhập</span>
-                            </li>
-                        </ul>
+        <>
+            <div className="header-top">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-6 header-top-left">
+                            <ul>
+                                <li><AiOutlineMail />Shoprauqua@gmail.com</li>
+                                <li>Miễn phí giao hàng tận nơi!</li>
+                            </ul>
+                        </div>
+                        <div className="col-6 header-top-right">
+                            <ul>
+                                <li><a href=""><AiFillFacebook /></a></li>
+                                <li><a href=""><AiOutlineInstagram /></a></li>
+                                <li><a href=""><AiFillGooglePlusCircle /></a></li>
+                                <li><a href=""><AiFillTwitterSquare /></a></li>
+                                <li>
+                                    <a href=""><AiOutlineUser /></a>
+                                    <span> Đăng nhập</span>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-xl-3">
+                        <div className="header_logo">
+                            <h1>My Shop</h1>
+                        </div>
+                    </div>
+                    <div className="col-xl-6">
+                        <nav className="header_menu">
+                            <ul>
+                                {
+                                    menus?.map((menu,menuKey) =>(
+                                        <li key={menuKey} className={menuKey ===0 ? "active" : ""}>
+                                            <a href={menu?.path}>{menu?.name}</a>
+                                            {
+                                                menu.child && (
+                                                    <ul className="header_menu_dropdown">
+                                                        {
+                                                            menu.child.map((childItem, childKey)=>(
+                                                                <li key={`${menuKey}-${childKey}`}>
+                                                                    <a href={childItem.path}>{childItem.name}</a>
+                                                                </li>
+                                                            ))
+                                                        }
+                                                    </ul>
+                                                )
+                                            }
+                                        </li>
+                                    ))
+                                }
+                            </ul>
+                        </nav>
+                    </div>
+                    <div className="col-xl-3">
+                        <div className="header_cart">
+                            <div className="header_cart_price">
+                                <span>{formatter(1111100)}</span>
+                            </div>
+                            <ul>
+                                <li>
+                                    <a href=""><AiOutlineShoppingCart /></a>
+                                    <span>5</span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 
 };
