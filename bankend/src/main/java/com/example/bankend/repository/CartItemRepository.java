6c7adc8c30df10b51101c489ec06d7bb89140ded
19130026 <107161736+19130026@users.noreply.github.com>
2024-05-26 18:47:54 +0700

    cập nhật giỏ hàng
    sửa lại thêm sản phẩm tránh bị trùng và hiện thị số sản phẩm đang trong giỏ hàng của người dùng

--- bankend/src/main/java/com/example/bankend/repository/CartItemRepository.java
+++ bankend/src/main/java/com/example/bankend/repository/CartItemRepository.java
@@ -1,7 +1,12 @@
 package com.example.bankend.repository;
 
+import com.example.bankend.entity.Cart;
 import com.example.bankend.entity.CartItem;
+import com.example.bankend.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    Optional<CartItem> findByCartAndProduct(Cart cart, Product product);
 }
--- bankend/src/main/java/com/example/bankend/service/CartService.java
+++ bankend/src/main/java/com/example/bankend/service/CartService.java
@@ -5,6 +5,10 @@ import com.example.bankend.entity.Cart;
 import com.example.bankend.entity.CartItem;
 import com.example.bankend.entity.User;
 
+import java.util.List;
+
 public interface CartService {
     CartItem addCartItem(User user, CartItemDTO cartItemDTO);
+    int getTotalQuantity(User user);
+
 }
