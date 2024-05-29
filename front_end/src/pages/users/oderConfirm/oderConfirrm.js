0358c4ab4faad5c4e8b86b30bad064837da36145
khai <19130094@st.hcmuaf.edu.vn>
2024-05-29 16:27:06 +0700

    xử lý xác nhận đơn hàng

--- front_end/src/pages/users/oderConfirm/oderConfirrm.js
+++ front_end/src/pages/users/oderConfirm/oderConfirrm.js
@@ -1,9 +1,8 @@
-import {memo, useEffect, useState} from "react";
+import { memo, useEffect, useState } from "react";
 import "./style.scss";
-import Item1 from "assets/users/image/trai_cay/dau_tay.jpg";
-import {formatter} from "../../../utils/formatter";
+import { formatter } from "../../../utils/formatter";
+import { useLocation } from "react-router-dom";
 import axios from "axios";
-import {useNavigate} from "react-router-dom";
 
 const getCartDetails = async () => {
     const API_URL = 'http://localhost:8080/api/cart/details';
@@ -15,123 +14,157 @@ const getCartDetails = async () => {
         throw error;
     }
 };
-const OrderConfirm = () =>{
 
-    const [cartItems, setCartItems] = useState([]);
-    const [total, setTotal] = useState(0);
-    const shinpMonney = 10000;
-    useEffect(() => {
-        const fetchCartDetails = async () => {
-            try {
-                const data = await getCartDetails();
-                setCartItems(data);
-            } catch (error) {
-                console.error('Error fetching cart details:', error);
-            }
-        };
+const OrderConfirm = () => {
+    // const [cartItems, setCartItems] = useState([]);
+    // const [total, setTotal] = useState(0);
 
-        fetchCartDetails();
-    }, []);
+    const location = useLocation();
+    const { orderDate, orderId, deliveryAddress,total,shippingMoney ,cartItems} = location.state || {};
 
-    useEffect(() => {
-        let totalAmount = 0;
-        cartItems.forEach(item => {
-            totalAmount += item.price * item.quantity;
-        });
-        setTotal(totalAmount);
-    }, [cartItems]);
+    // useEffect(() => {
+    //     const fetchCartDetails = async () => {
+    //         try {
+    //             const data = await getCartDetails();
+    //             setCartItems(data);
+    //         } catch (error) {
+    //             console.error('Error fetching cart details:', error);
+    //         }
+    //     };
+
+    //     fetchCartDetails();
+    // }, []);
+
+    // useEffect(() => {
+    //     let totalAmount = 0;
+    //     cartItems.forEach(item => {
+    //         totalAmount += item.price * item.quantity;
+    //     });
+    //     setTotal(totalAmount);
+    // }, [cartItems]);
 
     return (
-        <div class="container mt-5 mb-5">
-            <div class="row d-flex justify-content-center">
-                <div class="col-md-8">
-                    <div class="card">
-                        <div class="text-left logo p-2 px-5"><h1 style={{textAlign:`center`,padding:`25px`}}>Xác nhận đơn hàng!</h1> </div>
-                        <div class="invoice p-5">
-                            <span class="font-weight-bold d-block mt-4">Hello, </span> <span>Đơn đặt hàng của bạn đã được xác nhận và sẽ được giao trong vài ngày tới!</span>
-                            <div class="payment border-top mt-3 mb-3 border-bottom table-responsive">
-                                <table class="table table-borderless">
+        <div className="container mt-5 mb-5">
+            <div className="row d-flex justify-content-center">
+                <div className="col-md-8">
+                    <div className="card">
+                        <div className="text-left logo p-2 px-5">
+                            <h1 style={{ textAlign: `center`, padding: `25px` }}>Xác nhận đơn hàng!</h1>
+                        </div>
+                        <div className="invoice p-5">
+                            <span className="font-weight-bold d-block mt-4">Hello,</span>
+                            <span>Đơn đặt hàng của bạn đã được xác nhận và sẽ được giao trong vài ngày tới!</span>
+                            <div className="payment border-top mt-3 mb-3 border-bottom table-responsive">
+                                <table className="table table-borderless">
                                     <tbody>
                                     <tr>
                                         <td>
-                                            <div class="py-2"> <span class="d-block text-muted">Ngày đặt hàng</span> <br/> <span>20-11-2001</span> </div>
+                                            <div className="py-2">
+                                                <span className="d-block text-muted">Ngày đặt hàng</span>
+                                                <br />
+                                                <span>{orderDate}</span>
+                                            </div>
                                         </td>
                                         <td>
-                                            <div class="py-2"> <span class="d-block text-muted">Mã đặt hàng</span> <br/> <span>MT222</span> </div>
+                                            <div className="py-2">
+                                                <span className="d-block text-muted">Mã đặt hàng</span>
+                                                <br />
+                                                <span>{orderId}</span>
+                                            </div>
                                         </td>
                                         <td>
-                                            <div class="py-2"> <span class="d-block text-muted">Hình thức thanh toán</span> <br/><span>Thanh toán khi nhận hàng</span> </div>
+                                            <div className="py-2">
+                                                <span className="d-block text-muted">Hình thức thanh toán</span>
+                                                <br />
+                                                <span>Thanh toán khi nhận hàng</span>
+                                            </div>
                                         </td>
                                         <td>
-                                            <div class="py-2"> <span class="d-block text-muted">Địa chỉ</span> <br/><span>Bắc sơn , trảng bom , Đồng Nai</span> </div>
+                                            <div className="py-2">
+                                                <span className="d-block text-muted">Địa chỉ</span>
+                                                <br />
+                                                <span>{deliveryAddress}</span>
+                                            </div>
                                         </td>
                                     </tr>
                                     </tbody>
                                 </table>
                             </div>
-                            <div class="product border-bottom table-responsive">
-                                <table class="table table-borderless">
+                            <div className="product border-bottom table-responsive">
+                                <table className="table table-borderless">
                                     <tbody>
-                                    {cartItems.map((item,index)=>(
-                                        <tr>
-                                            <td><img style={{width: '70px',height:`70px`}} src={item.imageUrl} alt={item.productName} /></td>
+                                    {cartItems.map((item, index) => (
+                                        <tr key={index}>
+                                            <td><img style={{ width: '70px', height: `70px` }} src={item.imageUrl} alt={item.productName} /></td>
                                             <td>{item.productName}</td>
-                                            <td style={{width: '40%'}}><span className="font-weight-bold"></span>
-                                                <div className="product-qty"><span className="d-block" style={{
-                                                    color: `black`,
-                                                    fontSize: `14px`
-                                                }}>{item.quantity}</span></div>
+                                            <td style={{ width: '40%' }}>
+                                                <span className="font-weight-bold"></span>
+                                                <div className="product-qty">
+                                                    <span className="d-block" style={{ color: `black`, fontSize: `14px` }}>{item.quantity}</span>
+                                                </div>
                                             </td>
-                                            <td style={{width: '20%'}}>
-                                                <div className="text-right"><span
-                                                    className="font-weight-bold"> {formatter(item.quantity * item.price)}</span></div>
+                                            <td style={{ width: '20%' }}>
+                                                <div className="text-right">
+                                                    <span className="font-weight-bold"> {formatter(item.quantity * item.price)}</span>
+                                                </div>
                                             </td>
                                         </tr>
                                     ))}
-
-                                </tbody>
-                            </table>
-                        </div>
-                        <div class="row d-flex justify-content-end">
-                            <div class="col-md-5">
-                                <table class="table table-borderless">
-                                    <tbody class="totals">
-                                    <tr>
-                                        <td>
-                                            <div class="text-left"> <span class="text-muted">Tạm tính</span> </div>
-                                        </td>
-                                        <td>
-                                            <div class="text-right"> <span> {formatter(total)}</span> </div>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            <div class="text-left"> <span class="text-muted">Phí ship</span> </div>
-                                        </td>
-                                        <td>
-                                            <div class="text-right"> <span> {formatter(shinpMonney)}</span> </div>
-                                        </td>
-                                    </tr>
-                                    <tr class="border-top border-bottom">
-                                        <td>
-                                            <div class="text-left"> <span class="font-weight-bold">Tổng cộng</span> </div>
-                                        </td>
-                                        <td>
-                                            <div class="text-right"> <span class="font-weight-bold"> {formatter(total + shinpMonney)}</span> </div>
-                                        </td>
-                                    </tr>
                                     </tbody>
                                 </table>
                             </div>
+                            <div className="row d-flex justify-content-end">
+                                <div className="col-md-5">
+                                    <table className="table table-borderless">
+                                        <tbody className="totals">
+                                        <tr>
+                                            <td>
+                                                <div className="text-left">
+                                                    <span className="text-muted">Tạm tính</span>
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <div className="text-right">
+                                                    <span> {formatter(total)}</span>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <div className="text-left">
+                                                    <span className="text-muted">Phí ship</span>
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <div className="text-right">
+                                                    <span> {formatter(shippingMoney)}</span>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                        <tr className="border-top border-bottom">
+                                            <td>
+                                                <div className="text-left">
+                                                    <span className="font-weight-bold">Tổng cộng</span>
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <div className="text-right">
+                                                    <span className="font-weight-bold"> {formatter(total + shippingMoney)}</span>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                            <p>Chúng tôi sẽ gửi email xác nhận vận chuyển khi hàng được vận chuyển thành công!</p>
+                            <p className="font-weight-bold mb-0">Cảm ơn đã mua sắm với chúng tôi!</p>
                         </div>
-                        <p>Chúng tôi sẽ gửi email xác nhận vận chuyển khi hàng được vận chuyển thành công!</p>
-                        <p class="font-weight-bold mb-0">Cảm ơn đã mua sắm với chúng tôi!</p>
                     </div>
                 </div>
             </div>
         </div>
-</div>
-);
-
+    );
 };
+
 export default memo(OrderConfirm);
